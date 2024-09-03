a4f7908ac3b60bc2c6d59b742e31922b44857339
Tyron <lucena.tyronscott@gmail.com>
2024-09-03 19:02:26 +0800

    refactor: remove unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import * as rl from 'readline';
-import type { WebSocketData } from "./user";
-import { getStudentInfo, isColorValid, parseNameFromDlslEmail } from './utils';
 import { startServer } from './server';
 import { ask } from './debug';
 import { RedLightGreenLightRoom } from './redLightGreenLight';
